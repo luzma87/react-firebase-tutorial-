f67b1482fbd7dbba3e063d0862b0d1a07c8e5120
Luz <luzma_87@yahoo.com>
2019-09-10 17:48:57 -0500

    2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Tutorial from: [robinwieruch.de](https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React from 'react';
+import { withFirebase } from '../Firebase';
 
-const SignOut = () => (
-  <div>
-    SignOut
-  </div>
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
 );
-export default SignOut;
+
+export default withFirebase(SignOutButton);

177d52ac19cca43bf5f275332a1016c5bc58fd63
Luz <luzma_87@yahoo.com>
2019-09-10 18:00:17 -0500

    4

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react';
+import PasswordChangeForm from '../PasswordChange';
+import { PasswordForgetForm } from '../PasswordForget';
 
-const Account = () => (
-    <div>
-      Account
-    </div>
+const AccountPage = () => (
+  <div>
+    <h1>Account Page</h1>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
 );
-export default Account;
+export default AccountPage;
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-import { SignUpLink } from '../SignUp';
-import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
+import { PasswordForgetLink } from "../PasswordForget";
+import { SignUpLink } from '../SignUp';
 
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
@@ -22,29 +24,29 @@ const INITIAL_STATE = {
 class SignInFormBase extends Component {
   constructor(props) {
     super(props);
-    this.state = { ...INITIAL_STATE };
+    this.state = {...INITIAL_STATE};
   }
 
   onSubmit = event => {
-    const { email, password } = this.state;
+    const {email, password} = this.state;
     this.props.firebase
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
+        this.setState({...INITIAL_STATE});
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
-        this.setState({ error });
+        this.setState({error});
       });
     event.preventDefault();
   };
 
   onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({[event.target.name]: event.target.value});
   };
 
   render() {
-    const { email, password, error } = this.state;
+    const {email, password, error} = this.state;
     const isInvalid = password === '' || email === '';
     return (
       <form onSubmit={this.onSubmit}>

75c4acc2fc959b42dc343b350412343b1741cb26
Luz <luzma_87@yahoo.com>
2019-09-10 18:10:34 -0500

    5

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PasswordChangeForm from '../PasswordChange';
 import { PasswordForgetForm } from '../PasswordForget';
+import { AuthUserContext, withAuthorization } from '../Session';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
-export default AccountPage;
+
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);

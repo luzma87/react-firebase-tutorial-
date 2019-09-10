4ffb3e549861f96bac48686e250bb2fa40edcc8d
Luz <luzma_87@yahoo.com>
2019-09-10 18:23:22 -0500

    6

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import SignOutButton from "../SignOut";
 import { AuthUserContext } from '../Session';
+import SignOutButton from "../SignOut";
 
 const Navigation = () => (
   <div>
@@ -24,6 +24,9 @@ const NavigationAuth = () => (
     </li>
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ADMIN}>Admin</Link>
     </li>
     <li>
       <SignOutButton />
